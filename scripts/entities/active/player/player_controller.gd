2760503ec69d23051b49f360142fa4e7768453b0
tomspex <tboxtungsten@gmail.com>
2024-03-25 13:55:00 -0400

    dfjasdfjas

---
+++ scripts/entities/active/player/player_controller.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED:float = 300.0;
+
+func _physics_process(delta):
+	var direction:Vector2 = Input.get_vector("left", "right", "up", "down");
+	if(direction):
+		velocity = direction * Vector2(SPEED, SPEED);
+	else:
+		velocity = Vector2(move_toward(velocity.x, 0, SPEED), move_toward(velocity.y, 0, SPEED));
+
+	move_and_slide();
