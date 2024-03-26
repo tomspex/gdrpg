c1302ffebe956ac563525099f5047a9382d3b3df
tomspex <tboxtungsten@gmail.com>
2024-03-26 13:16:06 -0400

    finished player animation

--- scripts/entities/active/player/player_controller.gd
+++ scripts/entities/active/player/player_controller.gd
@@ -8,6 +8,10 @@ func _physics_process(delta):
 	var direction:Vector2 = Input.get_vector("left", "right", "up", "down");
 	velocity = direction * SPEED;
 	if(direction):
+		tree.get("parameters/playback").travel("walk");
+		tree.set("parameters/walk/blend_position", direction);
 		tree.set("parameters/idle/blend_position", direction);
+	else:
+		tree.get("parameters/playback").travel("idle");
 
 	move_and_slide();
