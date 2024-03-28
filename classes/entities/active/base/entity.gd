b60c74ec8b3b7a043525a46e074e8a9e97ab059e
tomspex <tboxtungsten@gmail.com>
2024-03-28 00:58:52 -0400

    i think i changed like a single sprite idk

--- classes/entities/active/base/entity.gd
+++
@@ -1,7 +0,0 @@
-extends Resource;
-
-class_name Entity;
-
-@export var display_name:String;
-@export var battle_sprite:Texture;
-@export var stats:EntityStats;
--- classes/entities/active/base/entity_stats.gd
+++
@@ -1,6 +0,0 @@
-extends Resource;
-
-class_name EntityStats;
-
-@export var health:int;
-@export var speed:int;
